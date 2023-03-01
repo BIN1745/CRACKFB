4e57404912b24605ba2b26fb4c96f00923d381d1
BIN1745 <nopriantodjaar@gmail.com>
2023-03-02 03:48:26 +0700

    first commit

---
+++ Install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#Dibuat OLeh ND
+#BLackWhiteTeam #BIN1745
+
+clear
+#color
+blue='\e[1;34m'
+green='\e[0;23m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+echo -e $cyan "Sedang Menginstall"
+sleep 3
+echo -e $red "TungguOke..."
+sleep 5
+clear
+apt update && apt upgrade -y
+clear
+apt install nmap -y
+clear
+apt install python -y
+clear
+apt install python2
+clear
+apt install wget -y
+clear
+apt install lolcat -y
+clear
+apt install figlet -y
+clear
+apt install cowsay -y
+clear
+gem install lolcat -y
+clear
+pip2 install enlighten -y
+clear
+apt install curl -y
+clear
+apt install php -y
+clear
+apt install git -y
+clear
+apt install python -y
+clear
+apt install python2 -y
+clear
+pip install requests
+clear
+pip install bs4
+clear
+pip2 install mechanize
+clear
+pip install mechanize
+clear
+pip install requests
+clear
+pip2 install requests
+clear
+pip2 install bs4
+clear
+pip install -r requirements.txt
+clear
+pip2 install -r requirements.txt
+clear
+chmod +x *
+
+
+clear
+sleep 2
+echo -e $red "[!]$purple Ketik Perintah $red [python TOKEN.py] $purple untuk Menjalankan Tool Ini"
+sleep 5
+echo -e $red "[!]$blue Sebelum masuk ke file silakan KETIKAN $red nano [caramain.txt]. Agar mudah memahami Tools"
+sleep 5
+echo ""
